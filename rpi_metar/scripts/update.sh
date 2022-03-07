75a53d98b4861d460dbe5c4bd3db7489f8e44a3d
Matt <matt.thompson1991@gmail.com>
2022-03-08 09:49:51 +1000

    Bring Dev branch up to Main

--- rpi_metar/scripts/update.sh
+++ rpi_metar/scripts/update.sh
@@ -1,12 +1,41 @@
+
 #!/bin/bash
 
-#Script to update rpi_metar_au to newest version on Main branch
-
-
+#Script to update rpi_metar_au to newest version on develop branch
 
 sudo systemctl stop rpi_metar_au
-sudo su
+sudo su <<EOF
 source /opt/rpi_metar_au/bin/activate
-pip install -U git+https://github.com/thommo17/rpi_metar_au.git@Main
+pip install -U git+https://github.com/thommo17/rpi_metar_au.git@develop
 exit
+EOF
 sudo systemctl start rpi_metar_au
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
